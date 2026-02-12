d76ea2ff3619deed54648272a41b3c4278f50221
Demid Travkov <d_travkov@kbtu.kz>
2026-02-13 03:52:24 +0500

    changed a few things

--- lab4/online-store/src/assets/products.ts
+++ lab4/online-store/src/assets/products.ts
@@ -41,7 +41,7 @@ export const PRODUCTS: Product[] = [
             "https://resources.cdn-kaspi.kz/img/m/p/p6c/pcb/44788874.png?format=gallery-medium",
             "https://resources.cdn-kaspi.kz/img/m/p/pfb/pca/44788870.png?format=gallery-medium"
         ],
-        "link": "https://kaspi.kz/shop/p/xiaomi-14-pro-256gb-sinii-145490077/?c=750000000"
+        "link": "https://kaspi.kz/shop/p/realme-c71-6-gb-128-gb-belyi-podarok-140357227/"
     },
     {
         "id": 4,
@@ -53,10 +53,95 @@ export const PRODUCTS: Product[] = [
         "images": [
             "https://resources.cdn-kaspi.kz/img/m/p/p4e/p53/30558289.png?format=gallery-medium",
             "https://resources.cdn-kaspi.kz/img/m/p/p2b/p56/30558294.png?format=gallery-medium",
-            "https://resources.cdn-kaspi.kz/img/m/p/pf3/p55/30558292.png?format=gallery-medium",
-            "https://resources.cdn-kaspi.kz/img/m/p/p08/pa1/30558302.png?format=gallery-medium"
+            "https://resources.cdn-kaspi.kz/img/m/p/pf3/p55/30558292.png?format=gallery-medium"
 
         ],
-        "link": "https://kaspi.kz/shop/p/huawei-p60-pro-512gb-zolotoi-145480088/?c=750000000"
-    }
+        "link": "https://kaspi.kz/shop/p/samsung-galaxy-a26-5g-8-gb-256-gb-zelenyi-137101301/?c=750000000"
+    },
+    {
+        "id": 5,
+        "name": "Смартфон Redmi A3x 3 ГБ/64 ГБ черный",
+        "description": "Смартфон Redmi A3x 3 ГБ/64 ГБ — это мощное устройство с высокой производительностью и стильным дизайном, которое идеально подходит для повседневного использования.",
+        "price": 39990,
+        "rating": 2.1,
+        "image": "https://resources.cdn-kaspi.kz/img/m/p/h83/h08/86585118720030.png?format=gallery-medium",
+        "images": [
+            "https://resources.cdn-kaspi.kz/img/m/p/h83/h08/86585118720030.png?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/h9a/h1b/86584697651230.jpg?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/hac/h2e/86584697683998.jpg?format=gallery-medium"
+        ],
+        "link": "https://kaspi.kz/shop/p/redmi-a3x-3-gb-64-gb-chernyi-121654928/?c=750000000"
+    },
+    {
+        "id": 6,
+        "name": "Смартфон Realme C71 6 ГБ/128 ГБ белый + подарок",
+        "description": "Realme C71 6 ГБ/128 ГБ — это мощный и стильный смартфон с впечатляющей автономностью и улучшенной защитой. В комплекте — беспроводные наушники Realme Buds T200 lite в подарок!",
+        "price": 74990,
+        "rating": 3.3,
+        "image": "https://resources.cdn-kaspi.kz/img/m/p/p3a/p8a/44788849.jpeg?format=gallery-medium",
+        "images": [
+            "https://resources.cdn-kaspi.kz/img/m/p/p3a/p8a/44788849.jpeg?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/p6c/pcb/44788874.png?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/pfb/pca/44788870.png?format=gallery-medium"
+        ],
+        "link": "https://kaspi.kz/shop/p/realme-c71-6-gb-128-gb-belyi-podarok-140357227/"
+    },
+    {
+        "id": 7,
+        "name": "Apple iPhone 17 Pro Max 256Gb оранжевый",
+        "description": "Apple iPhone 17 Pro Max 256Gb — это воплощение инноваций и стиля, предлагающее безупречную производительность и непревзойденные возможности для тех, кто всегда в движении.",
+        "price": 787500,
+        "rating": 4.9,
+        "image": "https://resources.cdn-kaspi.kz/img/m/p/p24/p88/64170090.png?format=gallery-medium",
+        "images": [
+            "https://resources.cdn-kaspi.kz/img/m/p/p24/p88/64170090.png?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/ped/p29/64476207.jpg?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/p41/p88/64170091.png?format=gallery-medium"
+        ],
+        "link": "https://kaspi.kz/shop/p/apple-iphone-17-pro-max-256gb-oranzhevyi-145468241/?c=750000000"
+    },
+    {
+        "id": 8,
+        "name": "Смартфон Samsung Galaxy A26 5G 8 ГБ/256 ГБ зеленый",
+        "description": "Samsung Galaxy A26 5G — это мощный и стильный смартфон с поддержкой сетей пятого поколения, который станет вашим надёжным помощником в повседневной жизни.",
+        "price": 589900,
+        "rating": 4.5,
+        "image": "https://resources.cdn-kaspi.kz/img/m/p/p4e/p53/30558289.png?format=gallery-medium",
+        "images": [
+            "https://resources.cdn-kaspi.kz/img/m/p/p4e/p53/30558289.png?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/p2b/p56/30558294.png?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/pf3/p55/30558292.png?format=gallery-medium",
+
+        ],
+        "link": "https://kaspi.kz/shop/p/samsung-galaxy-a26-5g-8-gb-256-gb-zelenyi-137101301/?c=750000000"
+    },
+    {
+        "id": 9,
+        "name": "Смартфон Realme C71 6 ГБ/128 ГБ белый + подарок",
+        "description": "Realme C71 6 ГБ/128 ГБ — это мощный и стильный смартфон с впечатляющей автономностью и улучшенной защитой. В комплекте — беспроводные наушники Realme Buds T200 lite в подарок!",
+        "price": 74990,
+        "rating": 3.3,
+        "image": "https://resources.cdn-kaspi.kz/img/m/p/p3a/p8a/44788849.jpeg?format=gallery-medium",
+        "images": [
+            "https://resources.cdn-kaspi.kz/img/m/p/p3a/p8a/44788849.jpeg?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/p6c/pcb/44788874.png?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/pfb/pca/44788870.png?format=gallery-medium"
+        ],
+        "link": "https://kaspi.kz/shop/p/realme-c71-6-gb-128-gb-belyi-podarok-140357227/"
+    },
+    {
+        "id": 10,
+        "name": "Смартфон Samsung Galaxy A26 5G 8 ГБ/256 ГБ зеленый",
+        "description": "Samsung Galaxy A26 5G — это мощный и стильный смартфон с поддержкой сетей пятого поколения, который станет вашим надёжным помощником в повседневной жизни.",
+        "price": 589900,
+        "rating": 4.5,
+        "image": "https://resources.cdn-kaspi.kz/img/m/p/p4e/p53/30558289.png?format=gallery-medium",
+        "images": [
+            "https://resources.cdn-kaspi.kz/img/m/p/p4e/p53/30558289.png?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/p2b/p56/30558294.png?format=gallery-medium",
+            "https://resources.cdn-kaspi.kz/img/m/p/pf3/p55/30558292.png?format=gallery-medium",
+
+        ],
+        "link": "https://kaspi.kz/shop/p/samsung-galaxy-a26-5g-8-gb-256-gb-zelenyi-137101301/?c=750000000"
+    },
 ];
