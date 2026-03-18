cd216de092cd8278ad2fa4520b5c910560287a04
Demid Travkov <d_travkov@kbtu.kz>
2026-03-19 01:37:43 +0500

    tons of updates, made views much cleaner + tested endpoints with postman

--- lab8/shopBack/api/serializers.py
+++ lab8/shopBack/api/serializers.py
@@ -6,13 +6,14 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
-    
-    def validate_price(self,value):
+    def validate_price(self, value):
         if value < 0:
-            raise serializers.ValidationError("Price must be at least 0")
+            raise serializers.ValidationError("Price must be positive")
         return value
+    
 
 class ProductCategorySerializer(serializers.ModelSerializer):
     class Meta: 
         model = ProductCategory
         fields = '__all__'
+    
--- lab8/shopBack/api/views.py
+++ lab8/shopBack/api/views.py
@@ -5,131 +5,91 @@ from rest_framework import status
 
 from .serializers import ProductCategorySerializer, ProductSerializer
 from .models import Product, ProductCategory
+from .utils import ResponseTemplates
 
 
-class BaseProductView:
-    def fetch_product_data(self,id):
-        product = None
+
+
+class BaseView(APIView):
+    model = None
+    serializer_class = None
+
+    def fetch_data(self,id):
+        entry = None
         try:
-            product = Product.objects.get(pk=id)
-        except:
-            product = None
+            entry = self.model.objects.get(pk=id)
+        except self.model.DoesNotExist:
+            entry = None
         
-        return product
+        return entry
     
 
-
-class ManyProductView(APIView, BaseProductView):
+class BaseManyView(BaseView):
     def get(self, request):
-        products_list = Product.objects.all()
-        serializer = ProductSerializer(products_list, many=True)
+        entry_list = self.model.objects.all()
+        serializer = self.serializer_class(entry_list, many=True)
 
-        return Response(serializer.data)
+        return ResponseTemplates.ok(data=serializer.data)
     
     def post(self, request):
-        serializer = ProductSerializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
+        else:
+            return ResponseTemplates.bad_request(errors=serializer.errors)
 
-        return Response(serializer.data)
-    
+        return ResponseTemplates.ok(data=[{"id" : serializer.data.get("id")}])
 
 
-class SingleProductView(APIView, BaseProductView):
+class BaseSingleView(BaseView):
     def get(self, request, id):
-        product = self.fetch_product_data(id)
+        entry = self.fetch_data(id)
 
-        if not product:
-            return Response(data={"status" : "N/A"}, status=status.HTTP_404_NOT_FOUND)
+        if not entry:
+            return ResponseTemplates.not_found()
         
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+        serializer = self.serializer_class(entry)
+        return ResponseTemplates.ok(data=[serializer.data])
     
     def put(self,request, id):
-        product = self.fetch_product_data(id)
+        entry = self.fetch_data(id)
 
-        if not product:
-            return Response(data={"status" : "N/A"}, status=status.HTTP_404_NOT_FOUND)
+        if not entry:
+            return ResponseTemplates.not_found()
         
-        serializer = ProductSerializer(product, data=request.data)
+        serializer = self.serializer_class(entry, data=request.data, partial=True)
 
         if serializer.is_valid():
             serializer.save()
-
-        return Response({"status" : "OK"}, status=status.HTTP_200_OK)
+        else:
+            return ResponseTemplates.bad_request(errors=serializer.errors)
+        return ResponseTemplates.ok(data=[{"id" : serializer.data.get("id")}])
         
     
     def delete(self, request, id):
-        product = self.fetch_product_data(id)
+        entry = self.fetch_data(id)
 
-        if not product:
-            return Response({"status" : "N/A"}, status=status.HTTP_404_NOT_FOUND)
+        if not entry:
+            return ResponseTemplates.not_found()
         
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        entry.delete()
+        return ResponseTemplates.no_content()
 
 
+class SingleProductView(BaseSingleView):
+    model = Product
+    serializer_class = ProductSerializer
+
+class ManyProductView(BaseManyView):
+    model = Product
+    serializer_class = ProductSerializer
 
 
-class BaseCategoryView:
-    def fetch_category_data(self,id):
-        product = None
-        try:
-            product = ProductCategory.objects.get(pk=id)
-        except:
-            product = None
-        
-        return product
-    
+class SingleProductCategoryView(BaseSingleView):
+    model = ProductCategory
+    serializer_class = ProductCategorySerializer
 
-
-class ManyCategoryView(APIView, BaseCategoryView):
-    def get(self, request):
-        category_list = ProductCategory.objects.all()
-        serializer = ProductCategorySerializer(category_list, many=True)
-
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = ProductCategorySerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-
-        return Response(serializer.data)
-    
-
-
-class SingleCategoryView(APIView, BaseCategoryView):
-    def get(self, request, id):
-        category = self.fetch_category_data(id)
-
-        if not category:
-            return Response(data={"status" : "N/A"}, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = ProductCategorySerializer(category)
-        return Response(serializer.data)
-    
-    def put(self,request, id):
-        category = self.fetch_category_data(id)
-
-        if not category:
-            return Response(data={"status" : "N/A"}, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = ProductCategorySerializer(category, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-
-        return Response({"status" : "OK"}, status=status.HTTP_200_OK)
-        
-    
-    def delete(self, request, id):
-        product = self.fetch_category_data(id)
-
-        if not product:
-            return Response({"status" : "N/A"}, status=status.HTTP_404_NOT_FOUND)
-        
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ManyProductCategoryView(BaseManyView):
+    model = ProductCategory
+    serializer_class = ProductCategorySerializer
